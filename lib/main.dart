eeab3b2480740450051d00b9ba31033543b87e99
Mansi Patil <50865213+mansi2804@users.noreply.github.com>
2024-10-24 02:01:29 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'views/yahtzee.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 
-
  
 void main() {
   runApp(const DiceGameApp());
